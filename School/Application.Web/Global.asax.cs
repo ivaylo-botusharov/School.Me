f3d286368bd6f614364b33814f7fa939ce0a4c24
encounter12 <encounter12@gmail.com>
2014-10-15 12:24:31 +0300

    AutoMapper configured, all ViewModels mapped to entities, Closes #2

--- School/Application.Web/Global.asax.cs
+++ School/Application.Web/Global.asax.cs
@@ -6,6 +6,7 @@
     using System.Web.Mvc;
     using System.Web.Optimization;
     using System.Web.Routing;
+    using Application.Web.AutoMapperConfig;
 
     public class MvcApplication : System.Web.HttpApplication
     {
@@ -16,6 +17,8 @@
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            AutoMapperWebConfiguration.Configure();
         }
     }
 }
